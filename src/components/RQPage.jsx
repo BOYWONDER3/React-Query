53d19760c14fd61cadb793f7024170f153d31473
Isaac Opeyemi Robert <80462715+BOYWONDER3@users.noreply.github.com>
2024-12-02 13:28:09 +0100

    fetching data with query

--- src/components/RQPage.jsx
+++ src/components/RQPage.jsx
@@ -1,9 +1,27 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 const RQpage = () => {
-    return (
-        <>
-            <h1>RQ PAGE</h1>
-        </>
-    )
-}
+  const { isLoading, data } = useQuery("super-heroes", fetchHeroes);
+
+  if (isLoading) {
+    return <h1>Loading......</h1>;
+  }
+
+  return (
+    <>
+      <h1>RQ PAGE</h1>
+      {data?.data.map((hero) => (
+        <ol key={hero.id}>
+          <li>{hero.name}</li>
+        </ol>
+      ))}
+    </>
+  );
+};
 
 export default RQpage;
