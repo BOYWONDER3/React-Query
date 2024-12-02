4e50e4b4b0f16f1a1501fd4e13a19ddd08cea38e
Isaac Opeyemi Robert <80462715+BOYWONDER3@users.noreply.github.com>
2024-12-03 00:05:39 +0100

    cache to query on click concept

--- src/components/RQPage.jsx
+++ src/components/RQPage.jsx
@@ -6,12 +6,18 @@ const fetchHeroes = () => {
 };
 
 const RQpage = () => {
-  const { isLoading, data, isError, error } = useQuery(
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
-    fetchHeroes
+    fetchHeroes,
+    {
+        enabled: false,
+    //   staleTime: 30000,
+    //   refetchInterval: 2000,
+    //   refetchIntervalInBackground: true,
+    }
   );
 
-  if (isLoading) {
+  if (isLoading || isFetching ) {
     return <h1>Loading......</h1>;
   }
   if (isError) return <h2>{error.message}</h2>;
@@ -19,6 +25,7 @@ const RQpage = () => {
   return (
     <>
       <h1>RQ PAGE</h1>
+      <button onClick={refetch}>Fetch Data</button>
       {data?.data.map((hero) => (
         <ol key={hero.id}>
           <li>{hero.name}</li>
